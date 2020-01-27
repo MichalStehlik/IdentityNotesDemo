fab46c5047e2d042335068b8b4c0da7c4350c3a9
Michal Stehlík <st@pslib.cloud>
2020-01-27 13:25:07 +0100

    Add project files.

---
+++ IdentityNotesDemo/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityNotesDemo.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ApplicationUser Owner { get; set; }
+        public string OwnerId { get; set; }
+    }
+}
